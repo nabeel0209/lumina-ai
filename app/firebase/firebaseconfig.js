af52ab9c456c5ebf3f64fc51a7ee7c03751e592c
nabeel0209 <muhammadnabeel0209@gmail.com>
2025-12-19 01:46:49 +0500

    Fixed API

--- app/firebase/firebaseconfig.js
+++ app/firebase/firebaseconfig.js
@@ -4,13 +4,15 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  authDomain: 'ai-chatbot-a8559.firebaseapp.com',
-  projectId: 'ai-chatbot-a8559',
-  storageBucket: 'ai-chatbot-a8559.firebasestorage.app',
-  messagingSenderId: '469548108186',
-  appId: '1:469548108186:web:2b003b2cca7e8efc725223',
-  measurementId: 'G-D50W5STWQD'
+  apiKey: "AIzaSyC0UyQjycAVC4_UjWtFmWlLN4Uw66mNLoc",
+  authDomain: "ai-chatbot-a8559.firebaseapp.com",
+  projectId: "ai-chatbot-a8559",
+  storageBucket: "ai-chatbot-a8559.firebasestorage.app",
+  messagingSenderId: "469548108186",
+  appId: "1:469548108186:web:2b003b2cca7e8efc725223",
+  measurementId: "G-D50W5STWQD"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
